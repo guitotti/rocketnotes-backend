4b92b9cdaec9d013f49b904d905be4f947b39b1b
Guilherme Totti <dev.guilhermetotti@gmail.com>
2023-11-28 23:00:00 -0300

    ✨ feat: image upload service

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,15 +1,19 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
+const multer = require("multer");
+const uploagConfig = require("../configs/upload");
 
-const UsersController = require("../controllers/UsersController")
+const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
-
-
+const upload = multer(uploagConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRoutes;
