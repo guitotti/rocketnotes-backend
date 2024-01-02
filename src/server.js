c29f84dc11aa7f5ff519770e576043ffc3a6121f
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-01-02 16:33:12 -0300

    ✨ feat: adding environment variables

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config");
 
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
@@ -34,7 +35,7 @@ app.use((error, request, response, next) => {
   })
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT} 🚀`);
 });
